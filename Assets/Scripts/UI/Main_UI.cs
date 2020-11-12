34c7acfb676bc8b645041f020acbd9a6817b8395
menew95 <menew95@hanmail.net>
2020-11-12 22:16:10 +0900

    15

--- Assets/Scripts/UI/Main_UI.cs
+++ Assets/Scripts/UI/Main_UI.cs
@@ -4,6 +4,30 @@ using UnityEngine;
 
 public class Main_UI : MonoBehaviour
 {
+    public RectTransform BG;
+
+    void OnEnable()
+    {
+
+    }
+
+    void OnDisable()
+    {
+
+    }
+
+    IEnumerator ScrollingBG()
+    {
+        // 360 랜덤
+
+        float ranDir = Random.Range(0, 360);
+
+        while (true)
+        {
+            yield return null;
+        }
+    }
+
     public void OnClickStartBtn()
     {
         UIManager.Instance.CallPracticeMenu();
